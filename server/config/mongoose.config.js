c28c507d3bafbaaae4a45f681f5649a5186a865e
Lourdes Valenzuela <126906933+LourdesValenzuela@users.noreply.github.com>
2024-07-25 18:07:55 -0400

    Update mongoose.config.js

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose'); // Importa el módulo Mongoose para interactuar con MongoDB.
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-// Conecta a la base de datos MongoDB en el puerto 27017 y a la base de datos 'crmdb'.
-mongoose.connect('mongodb://localhost:27017/crmdb', {
-    useNewUrlParser: true, // Usa el nuevo analizador de URL de MongoDB (evita advertencias de deprecación).
-    useUnifiedTopology: true // Usa el nuevo motor de topología unificada de MongoDB.
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/crmdb';
+
+mongoose.connect(dbUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
-    .then(() => console.log('Established a connection to the database')) // Mensaje en consola si la conexión es exitosa.
-    .catch(err => console.log('Something went wrong when connecting to the database', err)); // Maneja errores de conexión.
+    .then(() => console.log('Established a connection to the database'))
+    .catch(err => console.log('Something went wrong when connecting to the database', err));
